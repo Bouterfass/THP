cdaa8a6b3ee35b867ef79ce5c5e508b92da9d690
Youcef <cefyou28@gmail.com>
2020-10-12 15:03:02 +0200

    users and event class

---
+++ Jour_21/lib/user.rb
@@ -0,0 +1,24 @@
+require "pry"
+
+class User
+
+    attr_accessor :email, :age
+    @@users = {}
+
+    def initialize(email_to_save, age)
+        @email = email_to_save
+        @age = age 
+        @@users.store(email_to_save, age)
+    end
+
+    def self.print_users
+        return @@users
+    end
+
+    def self.find_by_email(email)
+         puts "L'utilisateur #{email} a #{@@users[email]} ans !"
+    end
+end
+
+
+binding pry
